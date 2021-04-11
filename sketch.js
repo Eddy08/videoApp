19088ab3386261a67ecf1567d9efb8e9c80ff093
Eddy08 <bhardwajharsh08@gmail.com>
2021-04-11 19:08:01 +0530

    Added Two More Keys 🔑🔐

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 let baseUrl="https://www.googleapis.com/youtube/v3/";
 // let key="key=AIzaSyBUr5hcuk4CKhKWrojKni2fd-Mvz-w10-c";
 let key="key=AIzaSyAOlzlNVdQJBg_s5WvXUT2fTEVcYPCDLkA";
-
+let key1="key=AIzaSyCE65vCleL5P4LSiBeQg-A8cRFJ8cSHmh4";//videoapp0008
+let key2="key=AIzaSyB1FD2jXYz3lHDW0FFYvuHgcKecXqShzzM";//videoapph0008
+let keyArray=[key,key1,key2];
+let debugURL=baseUrl;
 let maxResults=3;
 let part="snippet";
 let q="harsh";
@@ -19,8 +22,9 @@ function setup() {
   
     q=userInput.value();
     maxResults=maxResult.value();
-    let url =baseUrl+ "search?"+key+"&part="+part+"&maxResults="+maxResults+"&q="+q;
-    console.log(url)
+    let url =baseUrl+ "search?"+keyArray[Math.floor(Math.random()*keyArray.length)]+"&part="+part+"&maxResults="+maxResults+"&q="+q;
+    localStorage.setItem('debugURL',url); 
+    
     loadJSON(url,(data)=>{
       console.log(data);
       localStorage.setItem('api_data',JSON.stringify(data));
