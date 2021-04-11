ddf39f089340da7181b3e9c387183c4406638c88
Eddy08 <bhardwajharsh08@gmail.com>
2021-04-11 20:14:28 +0530

    New Key 🔐 Add Functionality 🆕

--- sketch.js
+++ sketch.js
@@ -10,30 +10,49 @@ let part="snippet";
 let q="harsh";
 let userInput;
 let gettingVideo;
+let newKey="newKey";
 let newResult="📷";
 let flag =0;
+let flag1=0; //for new key
 function setup() {
   // Main Method
+this. addNewKey= function(){
+  // console.log(newKey)
+  newKey=select('#newKeyValue');
+  localStorage.setItem('newKey',newKey.value());
+  console.log(newKey.value());
+}
 
 
  this.gettingVideo=function(){
-    userInput=select('#userInput');
-    maxResult=select('#userResult');
-  
-    q=userInput.value();
-    maxResults=maxResult.value();
-    let url =baseUrl+ "search?"+keyArray[Math.floor(Math.random()*keyArray.length)]+"&part="+part+"&maxResults="+maxResults+"&q="+q;
-    localStorage.setItem('debugURL',url); 
-    
-    loadJSON(url,(data)=>{
-      console.log(data);
-      localStorage.setItem('api_data',JSON.stringify(data));
-      newResult+=JSON.stringify(data);
-      
-      flag=1;
-    },'jsonp')
+  userInput = select('#userInput');
+  maxResult = select('#userResult');
+  console.log(maxResult)
+  q = userInput.value();
+  maxResults = maxResult.value();
 
-  }
+ let url=baseUrl;
+   if (localStorage.getItem('newKey')) {
+     newKeyLocally=localStorage.getItem('newKey');
+     newKeys=newKeyLocally;
+     url = baseUrl + "search?&key=" + newKeys + "&part=" + part + "&maxResults=" + maxResults + "&q=" + q;
+    localStorage.setItem('debugURL', url);
+  
+   }
+   else {
+     url = baseUrl + "search?" + keyArray[Math.floor(Math.random() * keyArray.length)] + "&part=" + part + "&maxResults=" + maxResults + "&q=" + q;
+    localStorage.setItem('debugURL', url);
+   }
+   loadJSON(url, (data) => {
+    console.log(data);
+    localStorage.setItem('api_data', JSON.stringify(data));
+    newResult += JSON.stringify(data);
+
+    flag = 1;
+  }, 'jsonp')
+ }
 
   
 }
+//test key AIzaSyAke72oaUVBnwithaOicHuYKJmWF3uGEhA
+//videotest8000
